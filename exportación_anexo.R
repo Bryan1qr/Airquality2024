46fd69c4f5400a628a23918f6f948ad78d020c26
Bryan1qr <bquisper@unjbg.edu.pe>
2025-03-05 11:11:14 -0500

    dm hoteles 2025

--- exportación_anexo.R
+++ exportación_anexo.R
@@ -7,16 +7,34 @@ library(zoo)
 library(glue)
 source("Scripts/ECA_mejorado.R")
 
-df <- ECA(meteo = "input/meteodiciembre2024.csv",
-          gases = "input/eca_mod_2024diciembre.csv",
-          pm = "input/pm_diciembre2024.csv",
-          fecha_inicio = "2024-12-01 00:00:00",
-          fecha_fin = "2024-12-31 23:00",
-          estacion = "Unanue",
+df <- ECA(meteo = "input/CR1000_FEBRERO_DM_2025.csv",
+          gases = "input/eca_2025_febrero.csv",
+          pm = "input/pm_DM_HOTELES2025.csv",
+          fecha_inicio = "2025-02-20 00:00",
+          fecha_fin = "2025-02-26 23:00",
+          estacion = "DM",
           tipo = "lista")
 
 df$df %>% 
   select(date, pm25, pm10, pres, pp, temp,
          hr, wd, ws, rad, no, no2, so2, h2s,
          co, o3_8h, co_8h) %>% 
-  write.xlsx("data_anexos/unanue_data_anexo.xlsx")
+  write.xlsx("data_anexos/DM_HOTELES_data_anexo.xlsx")
+
+
+library(openairmaps)
+dfm <- df$df %>% mutate(lat = -18.01429, lon = -70.24903,
+                        estacion = "DM HOTELES",
+                        XD = "Av. Francisco Bolognesi 300")
+
+windroseMap(data = dfm,
+            latitude = "lat",
+            longitude = "lon",
+            paddle = F,
+            d.icon = 180, popup = "estacion",
+            breaks = c(0,0.2,1.5,3.3,5.4),
+            include.lowest = FALSE,
+            cols = "viridis",
+            provider = c("OpenStreetMap", "Esri.WorldImagery"),
+            legend.title = "Velocidad<br>del viento (m/s)",
+            control.collapsed = TRUE, legend)
